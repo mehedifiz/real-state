75e79d1f61a5fd47c7c17872c54bb00e7875c0c0
mehedifiz <mehedi.shanto.contract@gamil.com>
2024-06-10 13:26:42 +0600

    fire api hide

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,13 +6,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCH6QZcB6LbMXT6mfNPxQHtthpyiGkLFqo",
-  authDomain: "urban-nest-b407c.firebaseapp.com",
-  projectId: "urban-nest-b407c",
-  storageBucket: "urban-nest-b407c.appspot.com",
-  messagingSenderId: "916577618994",
-  appId: "1:916577618994:web:97d68c90fa7b0273b6c02b"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
+
+console.log(import.meta.env.VITE_PASS)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
